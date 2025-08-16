617bb1200d7aabeecf5e3496ff27a5643b872552
regata69f <regata@yarzdev.com>
2025-08-16 13:24:53 +0700

    Create mount_position.lua

---
+++ mount_position.lua
@@ -0,0 +1,114 @@
+local Mount = {
+	["102234703920418"] = {
+		["Name"] = "Mount Daun",
+		["Teleports"] = {
+			["CP 1"] = {
+				["Position"] = Vector3.new(-621.723, 241.654, -383.892),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 60,
+			},
+			["CP 2"] = {
+				["Position"] = Vector3.new(-1203.186, 257.564, -487.081),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 60,
+			},
+			["CP 3"] = {
+				["Position"] = Vector3.new(-1399.288, 574.219, -949.934),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 60,
+			},
+			["CP 4"] = {
+				["Position"] = Vector3.new(-1701.048, 812.515, -1399.986),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 60,
+			},
+			["CP 5"] = {
+				["Position"] = Vector3.new(-3231.331, 1718.793, -2590.812),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 60,
+			},
+		},
+	},
+	["128473079243102"] = {
+		["Name"] = "Mount Arunika",
+		["Teleports"] = {
+			["CP 1"] = {
+				["Position"] = Vector3.new(135.679993, 144.275955, -174.722336),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+			["CP 2"] = {
+				["Position"] = Vector3.new(326.375885, 93.028038, -433.956573),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+			["CP 3"] = {
+				["Position"] = Vector3.new(476.093353, 172.284851, -940.202637),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+			["CP 4"] = {
+				["Position"] = Vector3.new(930.097229, 136.382202, -626.028931),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+			["CP 5"] = {
+				["Position"] = Vector3.new(923.416199, 103.369003, 279.597198),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+			["Summit"] = {
+				["Position"] = Vector3.new(246.178497, 321.047058, 704.100281),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 30,
+			},
+		},
+	},
+	["81708893434084"] = {
+		["Name"] = "Mount Yagataw",
+		["Teleports"] = {
+			["CP 1"] = {
+				["Position"] = Vector3.new(-421.25, 189.78125, 556.250061),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 2"] = {
+				["Position"] = Vector3.new(-625.972717, 577.270325, 1136.146),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 3"] = {
+				["Position"] = Vector3.new(-900.136047, 688.178711, 1221.91724),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 4"] = {
+				["Position"] = Vector3.new(-387.043945, 1039.75, 2046.20386),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 5"] = {
+				["Position"] = Vector3.new(-754.563843, 1523.47119, 2059.76953),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 6"] = {
+				["Position"] = Vector3.new(-479.396912, 1633, 2284.1665),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 7"] = {
+				["Position"] = Vector3.new(-85.3443604, 1929.25024, 2114.17407),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 8"] = {
+				["Position"] = Vector3.new(1.99995613, 2491.5, 2043.5),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+		},
+	}
+}
+
+return Mount
