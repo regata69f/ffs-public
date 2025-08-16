802d5eaf3c96663bc5aea78a04a7cfba6bc9c155
regata69f <regata@yarzdev.com>
2025-08-16 14:03:43 +0700

    Update mount_position.lua

--- mount_position.lua
+++ mount_position.lua
@@ -108,6 +108,276 @@ local Mount = {
 				["Delay"] = 3,
 			},
 		},
+	},
+	["106525193781380"] = {
+	 	["Name"] = "Mount Sibuatan",
+		["Teleports"] = {
+			["CP 1"] = {
+				["Position"] = Vector3.new(-313, 151.63134765625, -325.4570007324219),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 2"] = {
+				["Position"] = Vector3.new(-728, 586.0242309570312, -124),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 3"] = {
+				["Position"] = Vector3.new(-884.9541015625, 990.0433959960938, -205.75634765625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 4"] = {
+				["Position"] = Vector3.new(-1636.9541015625, 989.8005981445312, 282.24365234375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 5"] = {
+				["Position"] = Vector3.new(-1647.9541015625, 992.0433959960938, 633.24365234375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 6"] = {
+				["Position"] = Vector3.new(-1637.517333984375, 1109.9014892578125, 2151.49365234375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 7"] = {
+				["Position"] = Vector3.new(-519.1799926757812, 1446.03271484375, 3279.947998046875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 8"] = {
+				["Position"] = Vector3.new(-707.71630859375, 1889.963623046875, 2382.4365234375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 9"] = {
+				["Position"] = Vector3.new(-861.63232421875, 1937.7266845703125, 2070.189697265625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 10"] = {
+				["Position"] = Vector3.new(-867.9352416992188, 2098.170166015625, 1667.8934326171875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 11"] = {
+				["Position"] = Vector3.new(-901.63232421875, 2337.87890625, 1441.189697265625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 12"] = {
+				["Position"] = Vector3.new(-846.6904907226562, 2761.842041015625, 1505.594482421875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 13"] = {
+				["Position"] = Vector3.new(-614.6904907226562, 3282.0625, 1919.594482421875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 14"] = {
+				["Position"] = Vector3.new(-236.53936767578125, 3401.214111328125, 2579.06396484375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 15"] = {
+				["Position"] = Vector3.new(310.46063232421875, 3538.214111328125, 3043.06396484375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 16"] = {
+				["Position"] = Vector3.new(441.5592041015625, 3593.8671875, 3564),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 17"] = {
+				["Position"] = Vector3.new(912.3458862304688, 3662.21240234375, 4150.908203125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 18"] = {
+				["Position"] = Vector3.new(1422.7447509765625, 3901.6435546875, 4996.09521484375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 19"] = {
+				["Position"] = Vector3.new(1668, 4282.4375, 5192),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 20"] = {
+				["Position"] = Vector3.new(1721.294677734375, 4465.80615234375, 5171.2587890625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 21"] = {
+				["Position"] = Vector3.new(1865.01904296875, 4649.6689453125, 5200.1455078125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 22"] = {
+				["Position"] = Vector3.new(1902.294677734375, 4958.029296875, 5156.2587890625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 23"] = {
+				["Position"] = Vector3.new(2848.1904296875, 5069.8798828125, 5242.67626953125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 24"] = {
+				["Position"] = Vector3.new(3461.1904296875, 5237.8798828125, 5120.67626953125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 25"] = {
+				["Position"] = Vector3.new(4578.78466796875, 5474.22314453125, 5265.09912109375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 26"] = {
+				["Position"] = Vector3.new(4635.9814453125, 4945.9814453125, 5235.56884765625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 27"] = {
+				["Position"] = Vector3.new(4626.02734375, 4561.72265625, 5242.14599609375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 28"] = {
+				["Position"] = Vector3.new(5831.78076171875, 3993.63818359375, 5741.294921875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 29"] = {
+				["Position"] = Vector3.new(6631.78076171875, 4219.3349609375, 5578.294921875),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 30"] = {
+				["Position"] = Vector3.new(7478.55126953125, 4219.3203125, 5305.92236328125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 31"] = {
+				["Position"] = Vector3.new(8213.8115234375, 4326.3701171875, 4894.06787109375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 32"] = {
+				["Position"] = Vector3.new(8694.814453125, 4478, 4543.95361328125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 33"] = {
+				["Position"] = Vector3.new(8785.8115234375, 4533.84033203125, 4344.06787109375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 34"] = {
+				["Position"] = Vector3.new(9199.236328125, 5070.4375, 2465.622802734375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 35"] = {
+				["Position"] = Vector3.new(9188.9521484375, 5318.47216796875, 2459.5859375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 36"] = {
+				["Position"] = Vector3.new(9078, 5891.45654296875, 2041.046630859375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 37"] = {
+				["Position"] = Vector3.new(9188.1845703125, 6214.02294921875, 1988.2265625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 38"] = {
+				["Position"] = Vector3.new(9063.8330078125, 6494.71533203125, 1827.9962158203125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 39"] = {
+				["Position"] = Vector3.new(8693.8330078125, 6525.7265625, 1292.65478515625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 40"] = {
+				["Position"] = Vector3.new(8395.8330078125, 6553.7265625, 1140.1849365234375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 41"] = {
+				["Position"] = Vector3.new(7993.7890625, 6604.458984375, 1017.12890625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 42"] = {
+				["Position"] = Vector3.new(7138.26416015625, 6770.4375, 376.0508117675781),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 43"] = {
+				["Position"] = Vector3.new(6571.625, 6963.296875, 254),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 44"] = {
+				["Position"] = Vector3.new(6041, 6962.4375, 253),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 45"] = {
+				["Position"] = Vector3.new(4873, 7142.4375, 680.375),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 46"] = {
+				["Position"] = Vector3.new(5394.20947265625, 8105.8974609375, 2206.078369140625),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 47"] = {
+				["Position"] = Vector3.new(4878.94677734375, 3982.12841796875, 5181.0283203125),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+		},
+	},
+	["101006890582277"] = {
+		["Name"] = "Sumala Expedition",
+		["Teleports"] = {
+			["CP 1"] = {
+				["Position"] = Vector3.new(1897.67627, -441.014069, -2445.51587),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 2"] = {
+				["Position"] = Vector3.new(2359.67627, -253.533173, -2371.51587),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 3"] = {
+				["Position"] = Vector3.new(2896.59448, 10.5004988, -2326.30469),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["CP 4"] = {
+				["Position"] = Vector3.new(2757.03662, 562.421021, -2383.60718),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},
+			["Summit"] = {
+				["Position"] = Vector3.new(-3238.23633, 587, -3213.43872),
+				["Rotation"] = Vector3.new(0, 0, 0),
+				["Delay"] = 3,
+			},	
+		}
 	}
 }
 
