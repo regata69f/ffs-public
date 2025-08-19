b10440babe88f37b52a8ef5b64454aea244f5dce
regata69f <regata@yarzdev.com>
2025-08-19 11:50:43 +0700

    Create fish_list.lua

---
+++ fish_list.lua
@@ -0,0 +1,548 @@
+local Fishes = {
+	["Reef Chromis"] = {
+		Tier = 1,
+		Icon = "rbxassetid://130761855658346"
+	},
+	["Abyss Seahorse"] = {
+		Tier = 5,
+		Icon = "rbxassetid://140212951494890"
+	},
+	["Ash Basslet"] = {
+		Tier = 1,
+		Icon = "rbxassetid://89467763428838"
+	},
+	["Astra Damsel"] = {
+		Tier = 4,
+		Icon = "rbxassetid://80597542368624"
+	},
+	["Azure Damsel"] = {
+		Tier = 1,
+		Icon = "rbxassetid://82037285284063"
+	},
+	["Banded Butterfly"] = {
+		Tier = 2,
+		Icon = "rbxassetid://108599906664305"
+	},
+	["Blue Lobster"] = {
+		Tier = 4,
+		Icon = "rbxassetid://79569787883962"
+	},
+	["Blueflame Ray"] = {
+		Tier = 5,
+		Icon = "rbxassetid://113303332536600"
+	},
+	["Boa Angelfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://132840818442941"
+	},
+	["Bumblebee Grouper"] = {
+		Tier = 4,
+		Icon = "rbxassetid://138718017852833"
+	},
+	["Candy Butterfly"] = {
+		Tier = 2,
+		Icon = "rbxassetid://118627817589849"
+	},
+	["Charmed Tang"] = {
+		Tier = 3,
+		Icon = "rbxassetid://115703943090504"
+	},
+	["Chrome Tuna"] = {
+		Tier = 4,
+		Icon = "rbxassetid://103021560011734"
+	},
+	["Clownfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://98117815811053"
+	},
+	["Coal Tang"] = {
+		Tier = 2,
+		Icon = "rbxassetid://92488575546885"
+	},
+	["Copperband Butterfly"] = {
+		Tier = 2,
+		Icon = "rbxassetid://79518413256091"
+	},
+	["Corazon Damsel"] = {
+		Tier = 1,
+		Icon = "rbxassetid://117026260439113"
+	},
+	["Cow Clownfish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://88952725333000"
+	},
+	["Darwin Clownfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://109996187340520"
+	},
+	["Domino Damsel"] = {
+		Tier = 4,
+		Icon = "rbxassetid://100965955122629"
+	},
+	["Dorhey Tang"] = {
+		Tier = 4,
+		Icon = "rbxassetid://136891625122640"
+	},
+	["Dotted Stingray"] = {
+		Tier = 5,
+		Icon = "rbxassetid://132465838670740"
+	},
+	["Enchanted Angelfish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://108347802265821"
+	},
+	["Fire Goby"] = {
+		Tier = 2,
+		Icon = "rbxassetid://94533805203782"
+	},
+	["Firecoal Damsel"] = {
+		Tier = 4,
+		Icon = "rbxassetid://108870635586928"
+	},
+	["Flame Angelfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://128385926161840"
+	},
+	["Greenbee Grouper"] = {
+		Tier = 4,
+		Icon = "rbxassetid://79015880866732"
+	},
+	["Hammerhead Shark"] = {
+		Tier = 5,
+		Icon = "rbxassetid://91373020648344"
+	},
+	["Hawks Turtle"] = {
+		Tier = 5,
+		Icon = "rbxassetid://129453304232199"
+	},
+	["Starjam Tang"] = {
+		Tier = 4,
+		Icon = "rbxassetid://129447526017878"
+	},
+	["Jennifer Dottyback"] = {
+		Tier = 1,
+		Icon = "rbxassetid://82888113748973"
+	},
+	["Jewel Tang"] = {
+		Tier = 2,
+		Icon = "rbxassetid://130344776739450"
+	},
+	["Kau Cardinal"] = {
+		Tier = 3,
+		Icon = "rbxassetid://120735400809864"
+	},
+	["Korean Angelfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://134484917450451"
+	},
+	["Lavafin Tuna"] = {
+		Tier = 4,
+		Icon = "rbxassetid://96111934641941"
+	},
+	["Lobster"] = {
+		Tier = 4,
+		Icon = "rbxassetid://106973499045957"
+	},
+	["Loggerhead Turtle"] = {
+		Tier = 5,
+		Icon = "rbxassetid://91953712080535"
+	},
+	["Longnose Butterfly"] = {
+		Tier = 4,
+		Icon = "rbxassetid://78488362036598"
+	},
+	["Magic Tang"] = {
+		Tier = 4,
+		Icon = "rbxassetid://88573644059627"
+	},
+	["Magma Goby"] = {
+		Tier = 2,
+		Icon = "rbxassetid://136836819813966"
+	},
+	["Manta Ray"] = {
+		Tier = 5,
+		Icon = "rbxassetid://88385941559907"
+	},
+	["Maroon Butterfly"] = {
+		Tier = 4,
+		Icon = "rbxassetid://0"
+	},
+	["Maze Angelfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://133123622412997"
+	},
+	["Moorish Idol"] = {
+		Tier = 4,
+		Icon = "rbxassetid://99527607304877"
+	},
+	["Bandit Angelfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://86776001616210"
+	},
+	["Zoster Butterfly"] = {
+		Tier = 1,
+		Icon = "rbxassetid://80869011808607"
+	},
+	["Orangy Goby"] = {
+		Tier = 1,
+		Icon = "rbxassetid://80870534603019"
+	},
+	["Panther Grouper"] = {
+		Tier = 4,
+		Icon = "rbxassetid://110559683316227"
+	},
+	["Prismy Seahorse"] = {
+		Tier = 5,
+		Icon = "rbxassetid://105300029672501"
+	},
+	["Scissortail Dartfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://90918658215983"
+	},
+	["Shrimp Goby"] = {
+		Tier = 2,
+		Icon = "rbxassetid://79722493639079"
+	},
+	["Skunk Tilefish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://117649671337741"
+	},
+	["Specked Butterfly"] = {
+		Tier = 1,
+		Icon = "rbxassetid://116155560772333"
+	},
+	["Strawberry Dotty"] = {
+		Tier = 1,
+		Icon = "rbxassetid://113189737934915"
+	},
+	["Sushi Cardinal"] = {
+		Tier = 4,
+		Icon = "rbxassetid://104436232234256"
+	},
+	["Tricolore Butterfly"] = {
+		Tier = 2,
+		Icon = "rbxassetid://138769463668635"
+	},
+	["Unicorn Tang"] = {
+		Tier = 4,
+		Icon = "rbxassetid://85435653124474"
+	},
+	["Vintage Blue Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://136876564868613"
+	},
+	["Vintage Damsel"] = {
+		Tier = 2,
+		Icon = "rbxassetid://135465556501628"
+	},
+	["Volcanic Basslet"] = {
+		Tier = 1,
+		Icon = "rbxassetid://73441603866106"
+	},
+	["White Clownfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://94709558139061"
+	},
+	["Yello Damselfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://125066072333378"
+	},
+	["Yellowfin Tuna"] = {
+		Tier = 4,
+		Icon = "rbxassetid://86565235696991"
+	},
+	["Yellowstate Angelfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://82807541331151"
+	},
+	["Salmon"] = {
+		Tier = 2,
+		Icon = "rbxassetid://125238936461158"
+	},
+	["Blob Shark"] = {
+		Tier = 7,
+		Icon = "rbxassetid://120294742064292"
+	},
+	["Volsail Tang"] = {
+		Tier = 3,
+		Icon = "rbxassetid://129946055426678"
+	},
+	["Rockform Cardianl"] = {
+		Tier = 3,
+		Icon = "rbxassetid://120329148296828"
+	},
+	["Lava Butterfly"] = {
+		Tier = 2,
+		Icon = "rbxassetid://121441755541039"
+	},
+	["Slurpfish Chromis"] = {
+		Tier = 2,
+		Icon = "rbxassetid://73157781682607"
+	},
+	["Festive Goby"] = {
+		Tier = 1,
+		Icon = "rbxassetid://131719468046777"
+	},
+	["Mistletoe Damsel"] = {
+		Tier = 1,
+		Icon = "rbxassetid://119896827350397"
+	},
+	["Gingerbread Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://81165959957452"
+	},
+	["Great Christmas Whale"] = {
+		Tier = 7,
+		Icon = "rbxassetid://71302255255094"
+	},
+	["Gingerbread Clownfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://76627481663195"
+	},
+	["Gingerbread Turtle"] = {
+		Tier = 5,
+		Icon = "rbxassetid://105391647263950"
+	},
+	["Gingerbread Shark"] = {
+		Tier = 6,
+		Icon = "rbxassetid://134485196164902"
+	},
+	["Christmastree Longnose"] = {
+		Tier = 3,
+		Icon = "rbxassetid://121538254026074"
+	},
+	["Candycane Lobster"] = {
+		Tier = 4,
+		Icon = "rbxassetid://81306607650028"
+	},
+	["Festive Pufferfish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://111730193849173"
+	},
+	["Blue-Banded Goby"] = {
+		Tier = 2,
+		Icon = "rbxassetid://137187691311291"
+	},
+	["Blumato Clownfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://135433263876257"
+	},
+	["Conspi Angelfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://70666845671780"
+	},
+	["Fade Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://99713500880459"
+	},
+	["Lined Cardinal Fish"] = {
+		Tier = 5,
+		Icon = "rbxassetid://96973556889996"
+	},
+	["Masked Angelfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://85192354177842"
+	},
+	["Pygmy Goby"] = {
+		Tier = 1,
+		Icon = "rbxassetid://128173847216247"
+	},
+	["Sail Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://86089485683202"
+	},
+	["Watanabei Angelfish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://133021124365377"
+	},
+	["White Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://138516719984773"
+	},
+	["Ballina Angelfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://99236757363784"
+	},
+	["Bleekers Damsel"] = {
+		Tier = 2,
+		Icon = "rbxassetid://116977046936052"
+	},
+	["Loving Shark"] = {
+		Tier = 6,
+		Icon = "rbxassetid://119173750281399"
+	},
+	["Pink Smith Damsel"] = {
+		Tier = 2,
+		Icon = "rbxassetid://76606007357699"
+	},
+	["Axolotl"] = {
+		Tier = 4,
+		Icon = "rbxassetid://76558551981982"
+	},
+	["Silver Tuna"] = {
+		Tier = 2,
+		Icon = "rbxassetid://84055400571306"
+	},
+	["Pilot Fish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://114269547366510"
+	},
+	["Patriot Tang"] = {
+		Tier = 1,
+		Icon = "rbxassetid://118534942671075"
+	},
+	["Frostborn Shark"] = {
+		Tier = 7,
+		Icon = "rbxassetid://125463067542850"
+	},
+	["Plasma Shark"] = {
+		Tier = 5,
+		Icon = "rbxassetid://88847410678758"
+	},
+	["Pufferfish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://88950964359044"
+	},
+	["Racoon Butterfly Fish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://92680808637130"
+	},
+	["Orange Basslet"] = {
+		Tier = 2,
+		Icon = "rbxassetid://129792505755834"
+	},
+	["Strippled Seahorse"] = {
+		Tier = 5,
+		Icon = "rbxassetid://120444751052938"
+	},
+	["Thresher Shark"] = {
+		Tier = 5,
+		Icon = "rbxassetid://70681751795899"
+	},
+	["Great Whale"] = {
+		Tier = 7,
+		Icon = "rbxassetid://110349456710432"
+	},
+	["Worm Fish"] = {
+		Tier = 7,
+		Icon = "rbxassetid://88157067345872"
+	},
+	["Viperfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://132331063285672"
+	},
+	["Deep Sea Crab"] = {
+		Tier = 5,
+		Icon = "rbxassetid://107492220673765"
+	},
+	["Spotted Lantern Fish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://121295544969455"
+	},
+	["Robot Kraken"] = {
+		Tier = 7,
+		Icon = "rbxassetid://80927639907406"
+	},
+	["Monk Fish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://117683281700675"
+	},
+	["King Crab"] = {
+		Tier = 6,
+		Icon = "rbxassetid://136951139611035"
+	},
+	["Jellyfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://115477999636961"
+	},
+	["Giant Squid"] = {
+		Tier = 7,
+		Icon = "rbxassetid://135470649745250"
+	},
+	["Fangtooth"] = {
+		Tier = 4,
+		Icon = "rbxassetid://82285034746187"
+	},
+	["Electric Eel"] = {
+		Tier = 1,
+		Icon = "rbxassetid://112551199622795"
+	},
+	["Vampire Squid"] = {
+		Tier = 4,
+		Icon = "rbxassetid://87203317027314"
+	},
+	["Dark Eel"] = {
+		Tier = 2,
+		Icon = "rbxassetid://135880333888900"
+	},
+	["Boar Fish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://114009571858522"
+	},
+	["Blob Fish"] = {
+		Tier = 6,
+		Icon = "rbxassetid://109329387304752"
+	},
+	["Angler Fish"] = {
+		Tier = 4,
+		Icon = "rbxassetid://95565358831204"
+	},
+	["Dead Fish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://72777968915255"
+	},
+	["Skeleton Fish"] = {
+		Tier = 1,
+		Icon = "rbxassetid://110090152643341"
+	},
+	["Swordfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://105539179190905"
+	},
+	["Ghost Worm Fish"] = {
+		Tier = 7,
+		Icon = "rbxassetid://107896897205452"
+	},
+	["Ghost Shark"] = {
+		Tier = 7,
+		Icon = "rbxassetid://102028503382077"
+	},
+	["Rockfish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://88961987133136"
+	},
+	["Sheepshead Fish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://112711438552296"
+	},
+	["Blackcap Basslet"] = {
+		Tier = 2,
+		Icon = "rbxassetid://95659897738388"
+	},
+	["Catfish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://98970734819318"
+	},
+	["Coney Fish"] = {
+		Tier = 3,
+		Icon = "rbxassetid://126049387807805"
+	},
+	["Hermit Crab"] = {
+		Tier = 6,
+		Icon = "rbxassetid://84444153694943"
+	},
+	["Parrot Fish"] = {
+		Tier = 2,
+		Icon = "rbxassetid://101895900955177"
+	},
+	["Queen Crab"] = {
+		Tier = 7,
+		Icon = "rbxassetid://73709080100110"
+	},
+	["Red Snapper"] = {
+		Tier = 2,
+		Icon = "rbxassetid://70818316245817"
+	},
+}
+
+return Fishes
