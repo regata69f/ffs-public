2565cf8013a17a79d0e478fd124758f1942a5354
regata69f <regata@yarzdev.com>
2025-12-01 10:34:53 +0700

    Create ffs-conf.lua

---
+++ ffs-conf.lua
@@ -0,0 +1,28 @@
+return {
+    games = {
+	    [121864768012064] = '5ef4906f230aa87d747191682bd77c38', -- Fish It
+	    [71471877437404]      = '99f427dbe176af2932530ecbecb6d3dc', -- Cabin
+	    [110745840914242]     = '99f427dbe176af2932530ecbecb6d3dc', -- Cabin
+	    [127742093697776] = '48723aaade2692a66843c3f269c123e7', -- Plant Vs Brainrot
+	    [102234703920418] = "441b692d791ccc8add557367b35a6e2e", -- Mount Daun
+	    [128473079243102] = "441b692d791ccc8add557367b35a6e2e", -- Mount Arunika
+	    [81708893434084]  = "441b692d791ccc8add557367b35a6e2e", -- Mount Yagataw
+	    [106525193781380] = "441b692d791ccc8add557367b35a6e2e", -- Mount Sibuatan
+	    [101006890582277] = "441b692d791ccc8add557367b35a6e2e", -- Sumala Expedition
+	    [82004947373983]  = "441b692d791ccc8add557367b35a6e2e", -- Mount Yahayuk
+	    [123876706043500] = "441b692d791ccc8add557367b35a6e2e", -- Mount Horeg
+	    [138149789228609] = "441b692d791ccc8add557367b35a6e2e", -- Mount Rinjani
+	    [120818739452640] = "441b692d791ccc8add557367b35a6e2e", -- Mount Ckptw
+	    [123072895076939] = "441b692d791ccc8add557367b35a6e2e", -- Mount Haunt
+	    [2693023319]      = "441b692d791ccc8add557367b35a6e2e", -- Expedition Antarctica
+	    [75188411736711]  = "441b692d791ccc8add557367b35a6e2e", -- Mount Yaseterah
+	    [123224294054165] = "441b692d791ccc8add557367b35a6e2e", -- Mount Atin
+	    [123072895076939] = "441b692d791ccc8add557367b35a6e2e", -- Mount Hauk
+	    [73347831908825] = "441b692d791ccc8add557367b35a6e2e",  -- Hell Expedition
+	    [108448055225264] = "441b692d791ccc8add557367b35a6e2e", -- Mount Akhirat
+    },
+    keyLinks = {
+        {name = "Lootlabs (Recommended)", url = "https://ads.luarmor.net/get_key?for=FFS_Free_Keys-yTRpdogpOywa"},
+        {name = "Linkvertise (Alternative)", url = "https://ads.luarmor.net/get_key?for=FFS_Free_Keys_Workink-kuUgXaSxKuRT"},
+    }
+}
